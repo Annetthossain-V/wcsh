8fde21a58c2e2c81b81623d421ffe832f43a0778
Annett Hossain <annetthossain@outlook.com>
2025-10-10 22:52:52 +0600

    add default_variable

--- src/wcsh.cxx
+++ src/wcsh.cxx
@@ -1,9 +1,17 @@
 #include "core/shell_line.h"
 #include <spdlog/spdlog.h>
+#include "var/var.h"
 
-// keeping main very simple
+
+static void set_system_var() {
+  var::make_var("$VER", "0.2");
+  var::make_var("$SHELL", "wcsh");
+  var::make_var("$SH", "wcsh");
+}
+
 int main(int argc, char** argv) {
   // inits and stuff
+  set_system_var();
 
   line sh_line;
   while (true) {
@@ -21,6 +29,7 @@ int main(int argc, char** argv) {
     }
 
     sh_line.sys_exec();
+    set_system_var(); // performance problem
   }
 
   return 0;
